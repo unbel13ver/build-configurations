095a603a1234c865bd682b2e3267ee0365f43a30
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2022-12-21 13:57:11 +0200

    Testing
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- overlays/imx8qm.nix
+++ overlays/imx8qm.nix
@@ -3,7 +3,7 @@ final: prev: {
   linux_imx8 = final.callPackage ./bsp/kernel/linux-imx8 { pkgs = final; };
   inherit ( final.callPackage ./bsp/u-boot/imx8qm/imx-uboot.nix { pkgs = final; }) ubootImx8 imx-firmware;
 
-  linux_latest = final.linux_imx8;
+  kernel = final.linux_imx8;
 
   makeModulesClosure = args: prev.makeModulesClosure (args // {
     rootModules = [ "dm-verity" "loop" ];
